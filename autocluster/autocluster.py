45ed90dc54b28d91f908829ef197bde5a40c461e
wywongbd <wywongbd@gmail.com>
2019-07-04 10:46:53 +0800

    edited autocluster directory

---
+++ autocluster/autocluster.py
@@ -0,0 +1,14 @@
+from dataset import Dataset
+
+class AutoCluster(object):
+    def __init__(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def get_clusters(self):
+        pass
+    
+    def predict(self):
+        pass
---
+++ autocluster/dataset.py
@@ -0,0 +1,3 @@
+class Dataset(object):
+    def __init__(self):
+        pass
