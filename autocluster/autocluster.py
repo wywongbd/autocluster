95bcdd3daa3928bfbb2357c5fae624cc697b96fc
wywongbd <wywongbd@gmail.com>
2019-07-08 13:46:17 +0800

    written basic Dataset class

--- autocluster/autocluster.py
+++ autocluster/autocluster.py
@@ -2,13 +2,13 @@ from dataset import Dataset
 
 class AutoCluster(object):
     def __init__(self):
+        self.algorithm = None
+    
+    def fit(self, X, standardize=True):
+            
+    
+    def plot_clusters(self):
         pass
     
-    def fit(self):
-        pass
-    
-    def get_clusters(self):
-        pass
-    
-    def predict(self):
-        pass
+    def predict(self, X):
+        return self.algorithm.predict(X) if self.algorithm else None
