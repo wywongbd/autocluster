9963dfaae0a8e967f0cb2a5064b2b8ed9a0f88ee
wywongbd <wywongbd@gmail.com>
2019-07-09 18:24:08 +0800

    removed optimizer.py

--- autocluster/utils/stringutils.py
+++ autocluster/utils/stringutils.py
@@ -8,6 +8,6 @@ class StringUtils(object):
     @staticmethod
     def decode_parameter(encoded_parameter, algorithm_name):
         str_ls = encoded_parameter.split(Constants.parameter_algorithm_separator)
-        assert(str_ls[1] == algorithm_name)
+        assert(str_ls[-1] == algorithm_name)
         return str_ls[0]
         
