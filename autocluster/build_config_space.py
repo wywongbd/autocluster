8b4dac84b7f7f0d24fb14053139c976409f28078
wywongbd <wywongbd@gmail.com>
2019-07-09 11:48:28 +0800

    fixed build_config_space

--- autocluster/build_config_space.py
+++ autocluster/build_config_space.py
@@ -7,14 +7,13 @@ from ConfigSpace.conditions import InCondition
 def build_config_space(algorithms_ls=[algorithms.KMeans, algorithms.DBSCAN]):
     cs = ConfigurationSpace()
     algorithm_choice = CategoricalHyperparameter("algorithm_choice", 
-                                                 [alg.name for alg in algorithms_ls], 
-                                                 default_value=algorithms_ls[0].name)
+                                                 [alg for alg in algorithms_ls], 
+                                                 default_value=algorithms_ls[0])
     cs.add_hyperparameters([algorithm_choice])
     
     for algorithm in algorithms_ls:
-        print(algorithm)
         cs.add_hyperparameters(algorithm.params)
         for param in algorithm.params:
-            cs.add_condition(InCondition(child=param, parent=algorithm_choice, values=[algorithm.name]))
+            cs.add_condition(InCondition(child=param, parent=algorithm_choice, values=[algorithm]))
     
     return cs
