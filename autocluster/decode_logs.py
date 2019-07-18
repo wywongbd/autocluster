dfdda9c6a9dd33e7f98a0ea1ece1cbdce63d4ec9
wywongbd <wywongbd@gmail.com>
2019-07-18 16:15:46 +0800

    edited decode_logs.py

--- autocluster/decode_logs.py
+++ autocluster/decode_logs.py
@@ -2,29 +2,40 @@ import re
 
 class Decoder(object):
     @staticmethod
-    def read_file_as_string_ls(filepath):
-        with open(filepath, 'r') as f:
-            lines = f.read().splitlines()
-        return lines
+    def read_file_as_string(filepath):
+        with open(filepath, 'r') as myfile:
+            data = myfile.read()
+        return data
     
     @staticmethod
-    def split_logs_by_iteration(string_ls):
-        # this function returns a list of list of strings
-        # every list in the list belongs to one iteration
+    def split_logs_by_iteration(string):
+        # this function returns a list of strings
+        return re.findall("(ITERATION [0-9]{1} of [0-9]{1}[\s\S]{1,}?Done with ITERATION [0-9]{1})", string)
+    
+    @staticmethod
+    def get_records_by_iteration(string_ls):
+        # this function returns a list of dictionaries
+        return [eval(string.split('\n')[-2]) for string in string_ls]
+    
+    @staticmethod
+    def get_runhistory(string):
+        # the input string is the logged string from one iteration
+        string_ls =  re.findall("Fitting configuration:[\s\S]{1,}?Score obtained by this configuration:[\s\S]{1,}?\\n", string)
+        history = []
         
-        start_idx_ls = []
-        for i, string in enumerate(string_ls):
-            iteration_start = re.findall("ITERATION [0-9]{1} of [0-9]{1}", string)
+        for string in string_ls:
+            without_newline_ls = string.split('\n')
+            configuration = eval(without_newline_ls[1])
+            score_string = without_newline_ls[-2]
+            score_string = score_string[score_string.find('n:') + 2: ]
+            score = None
             
-            if len(iteration_start) > 0:
-                start_idx_ls.append(i)
-        
-        string_ls_ls = []
-        for i, idx in enumerate(start_idx_ls):
-            if i != len(start_idx_ls) - 1:
-                string_ls_ls.append(string_ls[idx : start_idx_ls[i + 1]])
+            if score_string.find('inf') != -1:
+                score = float('inf')
             else:
-                string_ls_ls.append(string_ls[idx: ])
-        
-        return string_ls_ls
+                score = eval(score_string)
+    
+            history.append((configuration, score))
+            
+        return history
             
