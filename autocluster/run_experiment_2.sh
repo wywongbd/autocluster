a3bd3609bbf1512b5e315d632dac6ff9a41b51f8
wywongbd <wywongbd@gmail.com>
2019-07-23 12:10:51 +0900

    edited run_experiment_2.sh but fifa19.csv still giving errors :(

--- autocluster/run_experiment_2.sh
+++ autocluster/run_experiment_2.sh
@@ -1,6 +1,8 @@
 # error encountered while running fifa19.csv
-# python metalearning.py --raw_data_path_ls ../data/breast_cancer.csv ../data/column_2C_weka.csv ../data/column_3C_weka.csv ../data/fifa19.csv ../data/Financial_Distress.csv ../data/hand_gestures_classification_merged.csv ../data/income_evaluation.csv ../data/mushrooms.csv ../data/pd_speech_features.csv --random_seed 27 --n_evaluations 200 --cutoff_time 1000
+# python metalearning.py --raw_data_path_ls ../data/breast_cancer.csv ../data/column_2C_weka.csv ../data/column_3C_weka.csv ../data/fifa19.csv ../data/Financial_Distress.csv ../data/hand_gestures_classification_merged.csv ../data/income_evaluation.csv ../data/mushrooms.csv ../data/pd_speech_features.csv --random_seed 27 --n_eva luations 200 --cutoff_time 1000
 
 # run remaining datasets
-python metalearning.py --raw_data_path_ls ../data/Financial_Distress.csv ../data/hand_gestures_classification_merged.csv ../data/income_evaluation.csv ../data/mushrooms.csv ../data/pd_speech_features.csv --random_seed 27 --n_evaluations 200 --cutoff_time 1000
+# python metalearning.py --raw_data_path_ls ../data/Financial_Distress.csv ../data/hand_gestures_classification_merged.csv ../data/income_evaluation.csv ../data/mushrooms.csv ../data/pd_speech_features.csv --random_seed 27 --n_evaluations 200 --cutoff_time 1000
 
+# rerun fifa19.csv, still encountering some errors .... :(
+python metalearning.py --raw_data_path_ls ../data/fifa19.csv --random_seed 27 --n_evaluations 200 --cutoff_time 1000
