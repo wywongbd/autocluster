89f65cba9d56f8fca34addc83d5c782c6f2315fd
wywongbd <wywongbd@gmail.com>
2019-07-31 22:36:53 +0900

    added scripts folder

---
+++ autocluster/scripts/run_test_autocluster_on_benchmark_1.sh
@@ -0,0 +1,37 @@
+cd ..
+
+# warmstart only
+python test_autocluster_on_benchmark.py \
+--benchmark_metafeatures_table_path metaknowledge/benchmark_silhouette_metafeatures_table.csv \
+--log_dir_prefix benchmark_experiment_1 \
+--test_size 0.1666 \
+--n_folds 3 \
+--random_seed 27 \
+--n_evaluations 25 \
+--cutoff_time 100 \
+--warmstart_n_neighbors 5 \
+--warmstart_top_n 5 
+
+# bayesian optimization only
+# python test_autocluster_on_benchmark.py \
+# --benchmark_metafeatures_table_path metaknowledge/benchmark_silhouette_metafeatures_table.csv \
+# --log_dir_prefix benchmark_experiment_2 \
+# --test_size 0.1666 \
+# --n_folds 3 \
+# --random_seed 27 \
+# --n_evaluations 100 \
+# --cutoff_time 100 \
+# --warmstart_n_neighbors 1 \
+# --warmstart_top_n 1 
+
+# warmstart + bayesian optimization
+# python test_autocluster_on_benchmark.py \
+# --benchmark_metafeatures_table_path metaknowledge/benchmark_silhouette_metafeatures_table.csv \
+# --log_dir_prefix benchmark_experiment_3 \
+# --test_size 0.1666 \
+# --n_folds 3 \
+# --random_seed 27 \
+# --n_evaluations 100 \
+# --cutoff_time 100 \
+# --warmstart_n_neighbors 5 \
+# --warmstart_top_n 5 
